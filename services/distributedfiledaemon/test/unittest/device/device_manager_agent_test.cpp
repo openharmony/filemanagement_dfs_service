23c966461e86b9849b72e9fa768770173957cffe
hhchinasoft <wangxianjun6@huawei.com>
2022-08-30 16:53:59 +0800

    增加filemanagement_dfs_service仓TDD用例
    Signed-off-by: hhchinasoft <wangxianjun6@huawei.com>

--- services/distributedfiledaemon/test/unittest/device/device_manager_agent_test.cpp
+++ services/distributedfiledaemon/test/unittest/device/device_manager_agent_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,14 +13,20 @@
  * limitations under the License.
  */
 
-#include <gtest/gtest.h>
+#include <exception>
+#include <memory>
+#include <unistd.h>
+
+#include "gtest/gtest.h"
 #include "device/device_manager_agent.h"
+#include "mountpoint/mount_point.h"
 
 namespace OHOS {
 namespace Storage {
 namespace DistributedFile {
 namespace Test {
 using namespace testing::ext;
+using namespace std;
 
 class DeviceManagerAgentTest : public testing::Test {
 public:
@@ -51,17 +57,175 @@ void DeviceManagerAgentTest::TearDown(void)
 }
 
 /**
- * @tc.name: integer_sub_001
- * @tc.desc: Verify the sub function.
+ * @tc.name: DeviceManagerAgentTest_Start_Stop_0100
+ * @tc.desc: Verify the Start/Stop function.
  * @tc.type: FUNC
- * @tc.require: Issue Number
+ * @tc.require: SR000H0387
  */
-HWTEST_F(DeviceManagerAgentTest, test_001, TestSize.Level1)
+HWTEST_F(DeviceManagerAgentTest, DeviceManagerAgentTest_Start_Stop_0100, TestSize.Level1)
 {
-    // step 1:调用函数获取结果
-    int actual = 4;
-    // Step 2:使用断言比较预期与实际结果
-    EXPECT_EQ(4, actual);
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_Start_Stop_0100 start";
+    auto sdm = DeviceManagerAgent::GetInstance();
+    bool res = true;
+
+    try {
+        sdm->Start();
+        sdm->Stop();
+    } catch (const exception &e) {
+        LOGE("Error:%{public}s", e.what());
+        res = false;
+    }
+
+    EXPECT_TRUE(res == false);
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_Start_Stop_0100 end";
+}
+
+/**
+ * @tc.name: DeviceManagerAgentTest_JoinGroup_QuitGroup_0100
+ * @tc.desc: Verify the JoinGroup/QuitGroup function.
+ * @tc.type: FUNC
+ * @tc.require: SR000H0387
+ */
+HWTEST_F(DeviceManagerAgentTest, DeviceManagerAgentTest_JoinGroup_QuitGroup_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_JoinGroup_QuitGroup_0100 start";
+    const int userId = 5202;
+    auto smp = make_shared<MountPoint>(Utils::DfsuMountArgumentDescriptors::Alpha(userId, "account"));
+    weak_ptr<MountPoint> wmp = smp;
+    auto sdm = DeviceManagerAgent::GetInstance();
+    bool res = true;
+
+    try {
+        sdm->JoinGroup(wmp);
+        sdm->QuitGroup(wmp);
+    } catch (const exception &e) {
+        LOGE("Error:%{public}s", e.what());
+        res = false;
+    }
+
+    EXPECT_TRUE(res == true);
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_JoinGroup_QuitGroup_0100 end";
+}
+
+/**
+ * @tc.name: DeviceManagerAgentTest_OnDeviceOnline_0100
+ * @tc.desc: Verify the OnDeviceOnline function.
+ * @tc.type: FUNC
+ * @tc.require: SR000H0387
+ */
+HWTEST_F(DeviceManagerAgentTest, DeviceManagerAgentTest_OnDeviceOnline_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceOnline_0100 start";
+    auto sdm = DeviceManagerAgent::GetInstance();
+    DistributedHardware::DmDeviceInfo deviceInfo;
+    bool res = true;
+
+    try {
+        sdm->OnDeviceOnline(deviceInfo);
+    } catch (const exception &e) {
+        LOGE("Error:%{public}s", e.what());
+        res = false;
+    }
+
+    EXPECT_TRUE(res == true);
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceOnline_0100 end";
+}
+
+/**
+ * @tc.name: DeviceManagerAgentTest_OnDeviceOffline_0100
+ * @tc.desc: Verify the OnDeviceOffline function.
+ * @tc.type: FUNC
+ * @tc.require: SR000H0387
+ */
+HWTEST_F(DeviceManagerAgentTest, DeviceManagerAgentTest_OnDeviceOffline_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceOffline_0100 start";
+    auto sdm = DeviceManagerAgent::GetInstance();
+    DistributedHardware::DmDeviceInfo deviceInfo;
+    bool res = true;
+
+    try {
+        sdm->OnDeviceOffline(deviceInfo);
+    } catch (const exception &e) {
+        LOGE("Error:%{public}s", e.what());
+        res = false;
+    }
+
+    EXPECT_TRUE(res == true);
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceOffline_0100 end";
+}
+
+/**
+ * @tc.name: DeviceManagerAgentTest_OnDeviceOffline_0200
+ * @tc.desc: Verify the OnDeviceOffline function.
+ * @tc.type: FUNC
+ * @tc.require: SR000H0387
+ */
+HWTEST_F(DeviceManagerAgentTest, DeviceManagerAgentTest_OnDeviceOffline_0200, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceOffline_0200 start";
+    auto sdm = DeviceManagerAgent::GetInstance();
+    DistributedHardware::DmDeviceInfo deviceInfo;
+    bool res = true;
+
+    try {
+        sdm->OnDeviceOnline(deviceInfo);
+        sdm->OnDeviceOffline(deviceInfo);
+    } catch (const exception &e) {
+        LOGE("Error:%{public}s", e.what());
+        res = false;
+    }
+
+    EXPECT_TRUE(res == true);
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceOffline_0200 end";
+}
+
+/**
+ * @tc.name: DeviceManagerAgentTest_OnDeviceChanged_0100
+ * @tc.desc: Verify the OnDeviceChanged function.
+ * @tc.type: FUNC
+ * @tc.require: SR000H0387
+ */
+HWTEST_F(DeviceManagerAgentTest, DeviceManagerAgentTest_OnDeviceChanged_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceChanged_0100 start";
+    auto sdm = DeviceManagerAgent::GetInstance();
+    DistributedHardware::DmDeviceInfo deviceInfo;
+    bool res = true;
+
+    try {
+        sdm->OnDeviceChanged(deviceInfo);
+    } catch (const exception &e) {
+        LOGE("Error:%{public}s", e.what());
+        res = false;
+    }
+
+    EXPECT_TRUE(res == true);
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceChanged_0100 end";
+}
+
+/**
+ * @tc.name: DeviceManagerAgentTest_OnDeviceReady_0100
+ * @tc.desc: Verify the OnDeviceReady function.
+ * @tc.type: FUNC
+ * @tc.require: SR000H0387
+ */
+HWTEST_F(DeviceManagerAgentTest, DeviceManagerAgentTest_OnDeviceReady_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceReady_0100 start";
+    auto sdm = DeviceManagerAgent::GetInstance();
+    DistributedHardware::DmDeviceInfo deviceInfo;
+    bool res = true;
+
+    try {
+        sdm->OnDeviceReady(deviceInfo);
+    } catch (const exception &e) {
+        LOGE("Error:%{public}s", e.what());
+        res = false;
+    }
+
+    EXPECT_TRUE(res == true);
+    GTEST_LOG_(INFO) << "DeviceManagerAgentTest_OnDeviceReady_0100 end";
 }
 } // namespace Test
 } // namespace DistributedFile
