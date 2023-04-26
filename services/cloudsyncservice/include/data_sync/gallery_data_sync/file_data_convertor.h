faaacd0104517c4541a89f454b0be9b09febecb2
jiahaoluo <luojiahao5@huawei.com>
2023-04-26 08:57:38 +0000

    add property and attachment conversion
    Signed-off-by: jiahaoluo <luojiahao5@huawei.com>
    Change-Id: I31db7d3f96f3da8f96ac3882b32927f051653a3d

--- services/cloudsyncservice/include/data_sync/gallery_data_sync/file_data_convertor.h
+++ services/cloudsyncservice/include/data_sync/gallery_data_sync/file_data_convertor.h
@@ -44,6 +44,29 @@ private:
     int32_t HandleFavorite(std::string &key, DriveKit::DKRecordData &data, NativeRdb::ResultSet &resultSet);
     int32_t HandleDescription(std::string &key, DriveKit::DKRecordData &data, NativeRdb::ResultSet &resultSet);
 
+    /* properties */
+    int32_t HandleProperties(std::string &key, DriveKit::DKRecordData &data, NativeRdb::ResultSet &resultSet);
+
+    int32_t HandleHeight(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleRotation(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleWidth(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandlePosition(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleDataModified(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleDetailTime(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleFileCreateTime(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleFirstUpdateTime(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleRelativeBucketId(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleSourceFileName(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleSourcePath(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+    int32_t HandleTimeZone(std::string &key, DriveKit::DKRecordFieldMap &map, NativeRdb::ResultSet &resultSet);
+
+    /* attachments */
+    int32_t HandleAttachments(std::string &key, DriveKit::DKRecordData &data, NativeRdb::ResultSet &resultSet);
+
+    int32_t HandleContent(DriveKit::DKRecordFieldList &list, NativeRdb::ResultSet &resultSet);
+    int32_t HandleThumbnail(DriveKit::DKRecordFieldList &list, NativeRdb::ResultSet &resultSet);
+    int32_t HandleLcd(DriveKit::DKRecordFieldList &list, NativeRdb::ResultSet &resultSet);
+
     /* identifier */
     int32_t userId_;
     std::string bundleName_;
@@ -58,6 +81,10 @@ private:
     /* map */
     static std::unordered_map<std::string, int32_t (FileDataConvertor::*)(std::string &key,
         DriveKit::DKRecordData &data, NativeRdb::ResultSet &resultSet)> map_;
+    static std::unordered_map<std::string, int32_t (FileDataConvertor::*)(std::string &key,
+        DriveKit::DKRecordData &data, NativeRdb::ResultSet &resultSet)> pMap_;
+    static std::unordered_map<std::string, int32_t (FileDataConvertor::*)(DriveKit::DKRecordFieldList &list,
+        NativeRdb::ResultSet &resultSet)> aMap_;
 };
 } // namespace CloudSync
 } // namespace FileManagement
