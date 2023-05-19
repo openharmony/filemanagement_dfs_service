617788c07e6c5095befefc9e1bb0cbbc85e91722
stonexx <shenyixuan@huawei.com>
2023-05-19 10:46:15 +0800

    download: refactoring download framework
    Change-Id: I24b0cec595cb6b4c23c8b6c7eea9adbe651bd3b7
    Signed-off-by: stonexx <shenyixuan@huawei.com>

---
+++ frameworks/native/cloudsync_kit_inner/include/cloud_download_callback_client.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_FILEMGMT_CLOUD_DOWNLOAD_CALLBACK_CLIENT_H
+#define OHOS_FILEMGMT_CLOUD_DOWNLOAD_CALLBACK_CLIENT_H
+
+#include "cloud_download_callback_stub.h"
+
+namespace OHOS::FileManagement::CloudSync {
+class CloudDownloadCallbackClient final : public CloudDownloadCallbackStub {
+public:
+    explicit CloudDownloadCallbackClient(const std::shared_ptr<CloudDownloadCallback> &callback)
+        : callback_(callback) {}
+
+    void OnDownloadedResult(int32_t result) override;
+    void OnDownloadProcess(int64_t downloadedSize, int64_t totalSize) override;
+
+private:
+    std::shared_ptr<CloudDownloadCallback> callback_;
+};
+} // namespace OHOS::FileManagement::CloudSync
+
+#endif // OHOS_FILEMGMT_CLOUD_DOWNLOAD_CALLBACK_CLIENT_H
---
+++ frameworks/native/cloudsync_kit_inner/src/cloud_download_callback_stub.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cloud_download_callback_stub.h"
+#include "dfs_error.h"
+#include "utils_log.h"
+
+namespace OHOS::FileManagement::CloudSync {
+using namespace std;
+
+CloudDownloadCallbackStub::CloudDownloadCallbackStub()
+{
+    opToInterfaceMap_[SERVICE_CMD_ON_DOWNLOADED] = &CloudDownloadCallbackStub::HandleOnDownloadedResult;
+    opToInterfaceMap_[SERVICE_CMD_ON_PROCESS] = &CloudDownloadCallbackStub::HandleOnProcess;
+}
+
+int32_t CloudDownloadCallbackStub::OnRemoteRequest(uint32_t code,
+                                                   MessageParcel &data,
+                                                   MessageParcel &reply,
+                                                   MessageOption &option)
+{
+    if (data.ReadInterfaceToken() != GetDescriptor()) {
+        return E_SERVICE_DESCRIPTOR_IS_EMPTY;
+    }
+    auto interfaceIndex = opToInterfaceMap_.find(code);
+    if (interfaceIndex == opToInterfaceMap_.end() || !interfaceIndex->second) {
+        LOGE("Cannot response request %d: unknown tranction", code);
+        return IPCObjectStub::OnRemoteRequest(code, data, reply, option);
+    }
+    return (this->*(interfaceIndex->second))(data, reply);
+}
+
+int32_t CloudDownloadCallbackStub::HandleOnDownloadedResult(MessageParcel &data, MessageParcel &reply)
+{
+    int32_t result = data.ReadInt32();
+    OnDownloadedResult(result);
+    return E_OK;
+}
+
+int32_t CloudDownloadCallbackStub::HandleOnProcess(MessageParcel &data, MessageParcel &reply)
+{
+    int64_t downloadedSize = data.ReadInt64();
+    int64_t totalSize = data.ReadInt64();
+    OnDownloadProcess(downloadedSize, totalSize);
+    return E_OK;
+}
+
+} // namespace OHOS::FileManagement::CloudSync
