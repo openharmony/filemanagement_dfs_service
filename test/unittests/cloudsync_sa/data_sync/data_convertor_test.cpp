83ec32eab5396153f59fc664842fadb23fa159d0
yinyong <yinyong14@huawei.com>
2023-05-25 14:45:59 +0800

    data_convertor_test END
    Signed-off-by: yinyong <yinyong14@huawei.com>

--- test/unittests/cloudsync_sa/data_sync/data_convertor_test.cpp
+++ test/unittests/cloudsync_sa/data_sync/data_convertor_test.cpp
@@ -67,7 +67,7 @@ public:
     MOCK_METHOD2(GetAsset, int(int32_t, ValueObject::Asset &));
     MOCK_METHOD2(GetAssets, int(int32_t, ValueObject::Assets &));
     MOCK_METHOD2(Get, int(int32_t, ValueObject &));
-    MOCK_METHOD1(GetModifyTime, int(std::string&));
+    MOCK_METHOD1(GetModifyTime, int(std::string &));
     MOCK_METHOD2(GetSize, int(int32_t, size_t &));
 };
 
@@ -80,7 +80,7 @@ public:
     void TearDown();
     std::shared_ptr<DataConvertorMock> dataConvertor_;
 
-    static const std::string DATABASE_NAME;
+    static const std::string databaseName;
     static std::shared_ptr<RdbStore> rdbStore_;
 };
 
@@ -88,10 +88,10 @@ class SqliteSharedOpenCallback : public RdbOpenCallback {
 public:
     int OnCreate(RdbStore &rdbStore) override;
     int OnUpgrade(RdbStore &rdbStore, int oldVersion, int newVersion) override;
-    static const std::string CREATE_TABLE_TEST;
+    static const std::string createTableTest;
 };
 
-std::string const SqliteSharedOpenCallback::CREATE_TABLE_TEST =
+std::string const SqliteSharedOpenCallback::createTableTest =
     "CREATE TABLE test (id INTEGER PRIMARY KEY AUTOINCREMENT, data1 TEXT,data2 INTEGER, data3 FLOAT, data4 BLOB);";
 
 int SqliteSharedOpenCallback::OnCreate(RdbStore &rdbStore)
@@ -109,7 +109,7 @@ const int ID1 = 1;
 const int ID2 = 2;
 const int ID3 = 3;
 static const std::string RDB_TEST_PATH = "/data/test/";
-const std::string DataConvertorTest::DATABASE_NAME = RDB_TEST_PATH + "dataConvertortest.db";
+const std::string DataConvertorTest::databaseName = RDB_TEST_PATH + "dataConvertortest.db";
 std::shared_ptr<RdbStore> DataConvertorTest::rdbStore_ = nullptr;
 
 void DataConvertorTest::GenerateDefaultTable()
@@ -145,20 +145,20 @@ void DataConvertorTest::GenerateDefaultTable()
 
 void DataConvertorTest::SetUpTestCase(void)
 {
-    RdbStoreConfig sqliteSharedRstConfig(DataConvertorTest::DATABASE_NAME);
+    RdbStoreConfig sqliteSharedRstConfig(DataConvertorTest::databaseName);
     SqliteSharedOpenCallback sqliteSharedRstHelper;
     int errCode = E_OK;
     DataConvertorTest::rdbStore_ =
         OHOS::NativeRdb::RdbHelper::GetRdbStore(sqliteSharedRstConfig, 1, sqliteSharedRstHelper, errCode);
     EXPECT_NE(DataConvertorTest::rdbStore_, nullptr);
-    DataConvertorTest::rdbStore_->ExecuteSql(SqliteSharedOpenCallback::CREATE_TABLE_TEST);
+    DataConvertorTest::rdbStore_->ExecuteSql(SqliteSharedOpenCallback::createTableTest);
     GenerateDefaultTable();
     GTEST_LOG_(INFO) << "SetUpTestCase";
 }
 
 void DataConvertorTest::TearDownTestCase(void)
 {
-    NativeRdb::RdbHelper::DeleteRdbStore(DataConvertorTest::DATABASE_NAME);
+    NativeRdb::RdbHelper::DeleteRdbStore(DataConvertorTest::databaseName);
     GTEST_LOG_(INFO) << "TearDownTestCase";
 }
 
@@ -301,5 +301,419 @@ HWTEST_F(DataConvertorTest, RecordToValueBucket002, TestSize.Level1)
         GTEST_LOG_(INFO) << " RecordToValueBucket002 ERROR";
     }
     GTEST_LOG_(INFO) << "RecordToValueBucket002 End";
+}
+
+/**
+ * @tc.name: GetInt001
+ * @tc.desc: Verify the GetInt function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetInt001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetInt001 Begin";
+    try {
+        std::string key;
+        int32_t val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetInt(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetInt001 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetInt001 End";
+}
+
+/**
+ * @tc.name: GetInt002
+ * @tc.desc: Verify the GetInt function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetInt002, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetInt002 Begin";
+    try {
+        std::string key;
+        int32_t val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetInt(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetInt(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetInt002 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetInt002 End";
+}
+
+/**
+ * @tc.name: GetInt003
+ * @tc.desc: Verify the GetInt function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetInt003, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetInt003 Begin";
+    try {
+        std::string key;
+        int32_t val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetInt(_, _)).WillOnce(Return(0));
+        int32_t ret = DataConvertor::GetInt(key, val, resultSet);
+        EXPECT_EQ(E_OK, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetInt003 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetInt003 End";
+}
+
+/**
+ * @tc.name: GetLong001
+ * @tc.desc: Verify the GetLong function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetLong001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetLong001 Begin";
+    try {
+        std::string key;
+        int64_t val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetLong(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetLong001 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetLong001 End";
+}
+
+/**
+ * @tc.name: GetLong002
+ * @tc.desc: Verify the GetLong function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetLong002, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetLong002 Begin";
+    try {
+        std::string key;
+        int64_t val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetLong(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetLong(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetLong002 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetLong002 End";
+}
+
+/**
+ * @tc.name: GetLong003
+ * @tc.desc: Verify the GetLong function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetLong003, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetLong003 Begin";
+    try {
+        std::string key;
+        int64_t val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetLong(_, _)).WillOnce(Return(0));
+        int32_t ret = DataConvertor::GetLong(key, val, resultSet);
+        EXPECT_EQ(E_OK, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetLong003 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetLong003 End";
+}
+
+/**
+ * @tc.name: GetDouble001
+ * @tc.desc: Verify the GetDouble function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetDouble001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetDouble001 Begin";
+    try {
+        std::string key;
+        double val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetDouble(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetDouble001 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetDouble001 End";
+}
+
+/**
+ * @tc.name: GetDouble002
+ * @tc.desc: Verify the GetDouble function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetDouble002, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetDouble002 Begin";
+    try {
+        std::string key;
+        double val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetDouble(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetDouble(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetDouble002 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetDouble002 End";
+}
+
+/**
+ * @tc.name: GetDouble003
+ * @tc.desc: Verify the GetDouble function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetDouble003, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetDouble003 Begin";
+    try {
+        std::string key;
+        double val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetDouble(_, _)).WillOnce(Return(0));
+        int32_t ret = DataConvertor::GetDouble(key, val, resultSet);
+        EXPECT_EQ(E_OK, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetDouble003 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetDouble003 End";
+}
+
+/**
+ * @tc.name: GetString001
+ * @tc.desc: Verify the GetString function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetString001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetString001 Begin";
+    try {
+        std::string key;
+        std::string val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetString(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetString001 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetString001 End";
+}
+
+/**
+ * @tc.name: GetString002
+ * @tc.desc: Verify the GetString function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetString002, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetString002 Begin";
+    try {
+        std::string key;
+        std::string val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetString(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetString(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetString002 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetString002 End";
+}
+
+/**
+ * @tc.name: GetString003
+ * @tc.desc: Verify the GetString function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetString003, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetString003 Begin";
+    try {
+        std::string key;
+        std::string val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetString(_, _)).WillOnce(Return(0));
+        int32_t ret = DataConvertor::GetString(key, val, resultSet);
+        EXPECT_EQ(E_OK, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetString003 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetString003 End";
+}
+
+/**
+ * @tc.name: GetBool001
+ * @tc.desc: Verify the GetBool function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetBool001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetBool001 Begin";
+    try {
+        std::string key;
+        bool val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetBool(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetBool001 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetBool001 End";
+}
+
+/**
+ * @tc.name: GetBool002
+ * @tc.desc: Verify the GetBool function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetBool002, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetBool002 Begin";
+    try {
+        std::string key;
+        bool val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetInt(_, _)).WillOnce(Return(1));
+        int32_t ret = DataConvertor::GetBool(key, val, resultSet);
+        EXPECT_EQ(E_RDB, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetBool002 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetBool002 End";
+}
+
+/**
+ * @tc.name: GetBool003
+ * @tc.desc: Verify the GetBool function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetBool003, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetBool003 Begin";
+    try {
+        std::string key;
+        bool val;
+        ResultSetMock resultSet;
+        EXPECT_CALL(resultSet, GetColumnIndex(_, _)).WillOnce(Return(0));
+        EXPECT_CALL(resultSet, GetInt(_, _)).WillOnce(Return(0));
+        int32_t ret = DataConvertor::GetBool(key, val, resultSet);
+        EXPECT_EQ(E_OK, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetBool003 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetBool003 End";
+}
+
+/**
+ * @tc.name: GetLongComp001
+ * @tc.desc: Verify the GetLongComp function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetLongComp001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetLongComp001 Begin";
+    try {
+        int64_t val;
+        DriveKit::DKRecordField field(std::string("1"));
+        int32_t ret = DataConvertor::GetLongComp(field, val);
+        EXPECT_EQ(E_OK, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetLongComp001 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetLongComp001 End";
+}
+
+/**
+ * @tc.name: GetLongComp002
+ * @tc.desc: Verify the GetLongComp function
+ * @tc.type: FUNC
+ * @tc.require: I6JPKG
+ */
+HWTEST_F(DataConvertorTest, GetLongComp002, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "GetLongComp002 Begin";
+    try {
+        int64_t val;
+        DriveKit::DKRecordField field(1);
+        int32_t ret = DataConvertor::GetLongComp(field, val);
+        EXPECT_EQ(E_OK, ret);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << " GetLongComp002 ERROR";
+    }
+
+    GTEST_LOG_(INFO) << "GetLongComp002 End";
 }
 } // namespace OHOS::FileManagement::CloudSync::Test
