9798f3cbdfbdf81d60ab29c0658cb3a5d6adbe2b
jiahaoluo <luojiahao5@huawei.com>
2023-06-15 06:55:30 +0000

    inline: fix TDD compile err
    Signed-off-by: jiahaoluo <luojiahao5@huawei.com>
    Change-Id: I8e04c2afc91cdb7cc9ca7b818b0a3f00ceafd793

--- services/cloudsyncservice/include/data_sync/gallery_data_sync/file_data_convertor.h
+++ services/cloudsyncservice/include/data_sync/gallery_data_sync/file_data_convertor.h
@@ -19,6 +19,9 @@
 #include <unordered_map>
 
 #include "data_convertor.h"
+#include "dfs_error.h"
+#include "gallery_file_const.h"
+#include "utils_log.h"
 
 namespace OHOS {
 namespace FileManagement {
@@ -109,6 +112,219 @@ private:
     static std::string thumb_suffix_;
     static std::string lcd_suffix_;
 };
+
+/* basic */
+inline int32_t FileDataConvertor::HandleAlbumId(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    data[key] = DriveKit::DKRecordField("default-album-1");
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleFileName(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    std::string val;
+    int32_t ret = GetString(Media::PhotoColumn::MEDIA_NAME, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    data[key] = DriveKit::DKRecordField(val);
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleHashId(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    data[key] = DriveKit::DKRecordField("1");
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleSource(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    data[key] = DriveKit::DKRecordField("");
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleFileType(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    DriveKit::DKRecordFieldMap properties;
+    auto err = data[FILE_PROPERTIES].GetRecordMap(properties);
+    if (err != DriveKit::DKLocalErrorCode::NO_ERROR) {
+        LOGE("get properties err %{public}d", err);
+        return E_DATA;
+    }
+    int32_t mediaType = properties[Media::MediaColumn::MEDIA_TYPE];
+    data[key] = DriveKit::DKRecordField(mediaType ==
+        Media::MEDIA_TYPE_VIDEO ? FILE_TYPE_VIDEO : FILE_TYPE_IMAGE);
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleCreatedTime(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    int64_t val;
+    int32_t ret = GetLong(Media::PhotoColumn::MEDIA_DATE_ADDED, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    /* no overflow: 64 >> 32 + 3 */
+    data[key] = DriveKit::DKRecordField(std::to_string(val * 1000));
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleFavorite(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    int32_t val;
+    int32_t ret = GetInt(Media::PhotoColumn::MEDIA_IS_FAV, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    data[key] = DriveKit::DKRecordField(val);
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleDescription(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    data[key] = DriveKit::DKRecordField("");
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleRecycle(const std::string &key, DriveKit::DKRecordData &data,
+    NativeRdb::ResultSet &resultSet)
+{
+    int64_t val;
+    int32_t ret = GetLong(Media::PhotoColumn::MEDIA_DATE_TRASHED, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    data[key] = DriveKit::DKRecordField(!!val);
+    return E_OK;
+}
+
+/* properties */
+inline int32_t FileDataConvertor::HandleHeight(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    int32_t val;
+    int32_t ret = GetInt(Media::PhotoColumn::PHOTO_HEIGHT, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    map[key] = DriveKit::DKRecordField(val);
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleRotation(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    int32_t val;
+    int32_t ret = GetInt(Media::PhotoColumn::PHOTO_ORIENTATION, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    map[key] = DriveKit::DKRecordField(val);
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleWidth(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    int32_t val;
+    int32_t ret = GetInt(Media::PhotoColumn::PHOTO_WIDTH, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    map[key] = DriveKit::DKRecordField(val);
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandlePosition(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    map[key] = DriveKit::DKRecordField("");
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleDataModified(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    int64_t val;
+    int32_t ret = GetLong(Media::PhotoColumn::MEDIA_DATE_MODIFIED, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    map[key] = DriveKit::DKRecordField(std::to_string(val));
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleDetailTime(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    map[key] = DriveKit::DKRecordField("");
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleFileCreateTime(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    int64_t val;
+    int32_t ret = GetLong(Media::PhotoColumn::MEDIA_DATE_ADDED, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    map[key] = DriveKit::DKRecordField(std::to_string(val));
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleFirstUpdateTime(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    map[key] = DriveKit::DKRecordField("");
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleRelativeBucketId(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    map[key] = DriveKit::DKRecordField("1");
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleSourceFileName(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    std::string val;
+    int32_t ret = GetString(Media::PhotoColumn::MEDIA_NAME, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    map[key] = DriveKit::DKRecordField(val);
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleSourcePath(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    std::string val;
+    int32_t ret = GetString(Media::PhotoColumn::MEDIA_FILE_PATH, val, resultSet);
+    if (ret != E_OK) {
+        return ret;
+    }
+    map[key] = DriveKit::DKRecordField(val);
+    return E_OK;
+}
+
+inline int32_t FileDataConvertor::HandleTimeZone(const std::string &key, DriveKit::DKRecordFieldMap &map,
+    NativeRdb::ResultSet &resultSet)
+{
+    map[key] = DriveKit::DKRecordField("");
+    return E_OK;
+}
 } // namespace CloudSync
 } // namespace FileManagement
 } // namespace OHOS
