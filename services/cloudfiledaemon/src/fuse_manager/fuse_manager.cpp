78f7442bc40e0e5b17e84d27645ea252184f017b
liuzerun <liuzerun@huawei.com>
2024-01-23 04:12:50 +0000

    bugfix
    Signed-off-by: liuzerun <liuzerun@huawei.com>

--- services/cloudfiledaemon/src/fuse_manager/fuse_manager.cpp
+++ services/cloudfiledaemon/src/fuse_manager/fuse_manager.cpp
@@ -454,7 +454,7 @@ static void CloudForgetMulti(fuse_req_t req, size_t count,
     fuse_reply_none(req);
 }
 
-static bool PrepareForRead(shared_ptr<char> &buf, size_t size, shared_ptr<CloudInode> cInode)
+static bool PrepareForRead(fuse_req_t req, shared_ptr<char> &buf, size_t size, shared_ptr<CloudInode> cInode)
 {
     if (size > MAX_READ_SIZE) {
         fuse_reply_err(req, EINVAL);
