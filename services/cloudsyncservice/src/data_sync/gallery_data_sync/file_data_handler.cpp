29854456f9ca505387e84ebdd5a9935448438be9
xuerunlong <xuerunlong1@huawei.com>
2024-03-26 20:23:08 +0800

    处理编辑报错
    Signed-off-by: xuerunlong <xuerunlong1@huawei.com>

--- services/cloudsyncservice/src/data_sync/gallery_data_sync/file_data_handler.cpp
+++ services/cloudsyncservice/src/data_sync/gallery_data_sync/file_data_handler.cpp
@@ -3774,8 +3774,8 @@ void FileDataHandler::UpdateThmVec()
         vector<int> fileIds = vector<int>(size);
         BatchGetFileIdFromCloudId(tmp, fileIds);
         ret = BatchUpdate(sql, PC::PHOTO_CLOUD_ID, tmp, count);
-        LOGI("update size is %{public}zu, success count is %{public}lld, fail count is %{public}zu",
-            size, count, tmp.size());
+        LOGI("update size is %{public}zu, success count is %{public}llu, fail count is %{public}zu",
+              static_cast<size_t>size, static_cast<unsigned long long>(count), tmp.size());
         if (ret != E_OK) {
             LOGW("update thm fail");
             std::lock_guard<std::mutex> lock(thmMutex_);
@@ -3814,8 +3814,8 @@ void FileDataHandler::UpdateLcdVec()
             lcdVec_.erase(lcdVec_.begin(), lcdVec_.begin() + size);
         }
         ret = BatchUpdate(sql, PC::PHOTO_CLOUD_ID, tmp, count);
-        LOGI("update size is %{public}zu, success count is %{public}lld, fail count is %{public}zu",
-            size, count, tmp.size());
+        LOGI("update size is %{public}zu, success count is %{public}llu, fail count is %{public}zu",
+              static_cast<size_t>size, static_cast<unsigned long long>(count), tmp.size());
         if (ret != E_OK) {
             LOGW("update thm fail");
             std::lock_guard<std::mutex> lock(lcdMutex_);
