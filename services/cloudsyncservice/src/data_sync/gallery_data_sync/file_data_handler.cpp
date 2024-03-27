6019e2e66e1f057590aa429b427ce11f19c5fa32
xuerunlong <xuerunlong1@huawei.com>
2024-03-27 09:58:33 +0800

    处理编辑报错
    Signed-off-by: xuerunlong <xuerunlong1@huawei.com>

--- services/cloudsyncservice/src/data_sync/gallery_data_sync/file_data_handler.cpp
+++ services/cloudsyncservice/src/data_sync/gallery_data_sync/file_data_handler.cpp
@@ -3774,7 +3774,7 @@ void FileDataHandler::UpdateThmVec()
         vector<int> fileIds = vector<int>(size);
         BatchGetFileIdFromCloudId(tmp, fileIds);
         ret = BatchUpdate(sql, PC::PHOTO_CLOUD_ID, tmp, count);
-        LOGI("update size is %{public}u, success count is %{public}lu, fail count is %{public}zu",
+        LOGI("update size is %{public}u, success count is %{public}llu, fail count is %{public}zu",
             size, count, tmp.size());
         if (ret != E_OK) {
             LOGW("update thm fail");
@@ -3814,7 +3814,7 @@ void FileDataHandler::UpdateLcdVec()
             lcdVec_.erase(lcdVec_.begin(), lcdVec_.begin() + size);
         }
         ret = BatchUpdate(sql, PC::PHOTO_CLOUD_ID, tmp, count);
-        LOGI("update size is %{public}u, success count is %{public}lu, fail count is %{public}zu",
+        LOGI("update size is %{public}u, success count is %{public}llu, fail count is %{public}zu",
             size, count, tmp.size());
         if (ret != E_OK) {
             LOGW("update thm fail");
