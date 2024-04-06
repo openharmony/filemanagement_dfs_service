9c08ec2dd203177bbed0da66aa5fd56783b2c18d
徐啟彬 <935159042@qq.com>
2024-04-06 16:09:47 +0800

    TicketNo:#I9E971 Description:解决编译arm64报错输出格式问题。 Signed-off-by: xuqibin <935159042@qq.com>

--- services/cloudsyncservice/src/data_sync/gallery_data_sync/file_data_handler.cpp
+++ services/cloudsyncservice/src/data_sync/gallery_data_sync/file_data_handler.cpp
@@ -3782,7 +3782,7 @@ void FileDataHandler::UpdateThmVec()
         vector<int> fileIds = vector<int>(size);
         BatchGetFileIdFromCloudId(tmp, fileIds);
         ret = BatchUpdate(sql, PC::PHOTO_CLOUD_ID, tmp, count);
-        LOGI("update size is %{public}zu, success count is %{public}lld, fail count is %{public}zu",
+        LOGI("update size is %{public}zu, success count is %{public}"  PRId64", fail count is %{public}zu",
             size, count, tmp.size());
         if (ret != E_OK) {
             LOGW("update thm fail");
@@ -3822,7 +3822,7 @@ void FileDataHandler::UpdateLcdVec()
             lcdVec_.erase(lcdVec_.begin(), lcdVec_.begin() + size);
         }
         ret = BatchUpdate(sql, PC::PHOTO_CLOUD_ID, tmp, count);
-        LOGI("update size is %{public}zu, success count is %{public}lld, fail count is %{public}zu",
+        LOGI("update size is %{public}zu, success count is %{public}" PRId64", fail count is %{public}zu",
             size, count, tmp.size());
         if (ret != E_OK) {
             LOGW("update thm fail");
