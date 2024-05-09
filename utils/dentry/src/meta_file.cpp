b9aad4eb3b154a25ff567f72266262f21ba06791
陈润 <chenrun5@huawei.com>
2024-05-09 08:02:09 +0000

    update utils/dentry/src/meta_file.cpp.
    Signed-off-by: 陈润 <chenrun5@huawei.com>

--- utils/dentry/src/meta_file.cpp
+++ utils/dentry/src/meta_file.cpp
@@ -530,7 +530,8 @@ static HmdfsDentry *FindInBlock(HmdfsDentryGroup &dentryBlk, uint32_t namehash, 
     return nullptr;
 }
 
-static HmdfsDentry *InLevel(uint32_t level, DcacheLookupCtx *ctx) __attribute__((no_sanitize("unsigned-integer-overflow")))
+static HmdfsDentry *InLevel(uint32_t level, DcacheLookupCtx *ctx) 
+                            __attribute__((no_sanitize("unsigned-integer-overflow")))
 {
     HmdfsDentry *de = nullptr;
 
