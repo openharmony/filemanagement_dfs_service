fc539858480caaf79cf021ad3a565f598996b386
yangli <yangli228@huawei.com>
2024-05-23 20:37:33 +0800

    修改删除时机
    Signed-off-by: yangli <“yangli228@huawei.com”>

--- services/distributedfiledaemon/src/network/softbus/softbus_session_listener.cpp
+++ services/distributedfiledaemon/src/network/softbus/softbus_session_listener.cpp
@@ -110,8 +110,8 @@ void SoftBusSessionListener::OnSessionOpened(int32_t sessionId, PeerSocketInfo i
     ret = ::SendFile(sessionId, src, dst, static_cast<uint32_t>(fileList.size()));
     if (ret != E_OK) {
         LOGE("SendFile failed, sessionId = %{public}d", sessionId);
-        SoftBusSessionPool::GetInstance().DeleteSessionInfo(sessionName);
     }
+    SoftBusSessionPool::GetInstance().DeleteSessionInfo(sessionName);
 }
 
 void SoftBusSessionListener::OnSessionClosed(int32_t sessionId, ShutdownReason reason)
