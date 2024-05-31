710d459966cb39d6fac56ad5964a88fb063f137a
魏子恒 <weiziheng4@huawei.com>
2024-05-31 13:25:26 +0000

    update interfaces/inner_api/native/cloud_file_kit_inner/network_set_manager.h.
    Signed-off-by: 魏子恒 <weiziheng4@huawei.com>

--- interfaces/inner_api/native/cloud_file_kit_inner/network_set_manager.h
+++ interfaces/inner_api/native/cloud_file_kit_inner/network_set_manager.h
@@ -12,40 +12,146 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "network_set_manager.h"
 
-#ifndef OHOS_FILEMGMT_CLOUD_SYNC_DATA_SHARE_H
-#define OHOS_FILEMGMT_CLOUD_SYNC_DATA_SHARE_H
-
-#include "datashare_helper.h"
-#include "data_ability_observer_stub.h"
-#include "data_sync_manager.h"
-#include <functional>
-#include <string>
+#include "accesstoken_kit.h"
+#include "cloud_file_kit.h"
+#include "datashare_errno.h"
+#include "datashare_result_set.h"
+#include "dfs_error.h"
+#include "ipc_skeleton.h"
+#include "parameters.h"
+#include "utils_log.h"
 
 namespace OHOS::FileManagement::CloudSync {
-class MobileNetworkObserver : public AAFwk::DataAbilityObserverStub {
-public:
-    MobileNetworkObserver() {}
-    ~MobileNetworkObserver() {}
-    void OnChange() override;
-    using ObserverCallback = std::function<void()>;
-private:
-    ObserverCallback observerCallback_ = nullptr;
-};
+const std::string QUERY_URI = "datashareproxy://";
+const std::string SWITCH_STATUS_KEY = "useMobileNetworkData";
+const std::string CLOUDDRIVE_KEY = "persist.kernel.bundle_name.clouddrive";
+const std::string BUNDLE_NAME_KEY = "key";
+const std::string BUNDLE_NAME_VALUE = "value";
+auto clouddriveBundleName = system::GetParameter(CLOUDDRIVE_KEY, "");
+auto queryUri = QUERY_URI + clouddriveBundleName + "/cloud_sp?key=" + SWITCH_STATUS_KEY;
+std::map<std::string, bool> resultMap;
+DataShare::CreateOptions options;
 
-class NetworkSetManager {
-public:
-    static void InitNetworkSetManager(const std::string &bundleName, const int32_t userId);
-    static int32_t QueryByDataShare(int32_t userId, const std::string &bundleName);
-    static void RegisterObserver();
-    static void UnregisterObserver();
-    static void GetNetworkSetting(int32_t userId, const std::string &bundleName);
-    static bool IsNetworkSetStatusOkay(const std::string &bundleName, const int32_t userId);
-    static bool GetConfigParams(const std::string &bundleName, const int32_t userId);
-    static inline bool networkSetStatus_;
-    static inline int32_t userId_;
-    static inline std::string bundleName_;
-};
-} // OHOS
+int32_t NetworkSetManager::QueryByDataShare(int32_t userId, const std::string &bundleName)
+{
+    LOGI("Query NetworkSetManager status");
+    DataShare::DataSharePredicates predicates;
+    predicates.EqualTo(BUNDLE_NAME_KEY, SWITCH_STATUS_KEY);
+    std::shared_ptr<DataShare::DataShareResultSet> resultSet;
+    std::vector<std::string> columns;
+    DataShare::CreateOptions options;
+    options.enabled_ = true;
+    auto clouddriveBundleName = system::GetParameter(CLOUDDRIVE_KEY, "");
+    auto queryUri = QUERY_URI + clouddriveBundleName + "/cloud_sp?user=" + std::to_string(userId);
+    Uri uri(queryUri);
+    auto dataShareHelper = DataShare::DataShareHelper::Creator(QUERY_URI, options);
+    if (dataShareHelper == nullptr) {
+        LOGE("dataShareHelper = nullptr");
+        return E_RDB;
+    }
+    resultSet = dataShareHelper->Query(uri, predicates, columns);
+    if (resultSet == nullptr) {
+        return E_RDB;
+    }
+    if (resultSet->GoToFirstRow() != E_OK) {
+        return E_RDB;
+    }
+    int32_t columnIndex = 0;
+    if (resultSet->GetColumnIndex(BUNDLE_NAME_VALUE, columnIndex) != E_OK) {
+        return E_RDB;
+    }
+    int64_t status = -1;
+    if (resultSet->GetLong(columnIndex, status) != E_OK) {
+        return E_RDB;
+    }
+    if (status == 1) {
+        resultMap[std::to_string(userId) + "/" + bundleName] = true;
+        return E_OK;
+    } else {
+        resultMap[std::to_string(userId) + "/" + bundleName] = false;
+        return E_OK;
+    }
+}
 
-#endif // OHOS_FILEMGMT_CLOUD_SYNC_DATA_SHARE_H
+void NetworkSetManager::GetNetworkSetting(int32_t userId, const std::string &bundleName)
+{
+    if (QueryByDataShare(userId, bundleName) != E_OK) {
+        resultMap[std::to_string(userId) + "/" + bundleName] = GetConfigParams(bundleName, userId);
+    }
+}
+
+bool NetworkSetManager::GetConfigParams(const std::string &bundleName, int32_t userId)
+{
+    auto driveKit = CloudFile::CloudFileKit::GetInstance();
+    std::map<std::string, std::string> param;
+    auto err = driveKit->GetAppConfigParams(userId, bundleName, param);
+    if (err != E_OK || param.empty()) {
+        LOGE("GetAppConfigParams failed");
+        return false;
+    }
+    int32_t networkData = std::stoi(param["useMobileNetworkData"]);
+    if (networkData == 0) {
+        return false;
+    }
+    return true;
+}
+
+void NetworkSetManager::UnregisterObserver()
+{
+    options.enabled_ = true;
+    auto dataShareHelper = DataShare::DataShareHelper::Creator(QUERY_URI, options);
+    Uri observerUri(queryUri);
+    if (dataShareHelper == nullptr) {
+        LOGE("dataShareHelper = nullptr");
+        return;
+    }
+    sptr<MobileNetworkObserver> dataObserver(new (std::nothrow) MobileNetworkObserver());
+    dataShareHelper->UnregisterObserver(observerUri, dataObserver);
+    LOGI("UnRegisterObserver finish");
+}
+
+void NetworkSetManager::RegisterObserver()
+{
+    options.enabled_ = true;
+    auto dataShareHelper = DataShare::DataShareHelper::Creator(QUERY_URI, options);
+    Uri observerUri(queryUri);
+    if (dataShareHelper == nullptr) {
+        LOGE("dataShareHelper = nullptr");
+        return;
+    }
+    sptr<MobileNetworkObserver> dataObserver(new (std::nothrow) MobileNetworkObserver());
+    dataShareHelper->RegisterObserver(observerUri, dataObserver);
+    LOGI("RegisterObserver finish");
+}
+
+void MobileNetworkObserver::OnChange()
+{
+    LOGI("MobileNetworkObserver OnChange network status");
+    if (observerCallback_ != nullptr) {
+        observerCallback_();
+    }
+    NetworkSetManager::GetNetworkSetting(NetworkSetManager::userId_, NetworkSetManager::bundleName_);
+}
+
+bool NetworkSetManager::IsNetworkSetStatusOkay(const std::string &bundleName, const int32_t userId)
+{
+    LOGI("IsNetworkSetStatusOkay start");
+    bool checkstatus = resultMap[std::to_string(userId) + "/" + bundleName];
+    if (checkstatus) {
+        LOGI("IsNetworkSetStatusOkay checkstatus = true");
+        return true;
+    }
+    return false;
+}
+
+void NetworkSetManager::InitNetworkSetManager(const std::string &bundleName, const int32_t userId)
+{
+    LOGI("InitNetworkSetManager start");
+    RegisterObserver();
+    bundleName_ = bundleName;
+    userId_ = userId;
+    GetNetworkSetting(userId, bundleName);
+}
+}
