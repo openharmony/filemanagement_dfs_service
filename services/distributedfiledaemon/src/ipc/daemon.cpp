b4195e38573dff2e2bbd279b022d53b308a446dc
liuxiaowei <liuxiaowei45@huawei.com>
2024-08-06 09:29:52 +0800

    修改
    Signed-off-by: liuxiaowei <liuxiaowei45@huawei.com>

--- services/distributedfiledaemon/src/ipc/daemon.cpp
+++ services/distributedfiledaemon/src/ipc/daemon.cpp
@@ -478,7 +478,7 @@ int32_t Daemon::GetRealPath(const std::string &srcUri,
         return E_GET_PHYSICAL_PATH_FAILED;
     }
 
-    LOGI("physicalPath %{public}s, userId {public}s", GetAnonyString(physicalPath).c_str(),
+    LOGI("physicalPath %{public}s, userId %{public}s", GetAnonyString(physicalPath).c_str(),
          std::to_string(hapTokenInfo.userID).c_str());
     info.dstPhysicalPath = physicalPath;
     ret = CheckCopyRule(physicalPath, dstUri, hapTokenInfo, isSrcFile, info);
