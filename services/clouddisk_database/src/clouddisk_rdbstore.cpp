417365ed6be1619ca7da4ff7a1187b0c77a8d122
xuerunlong <xuerunlong1@huawei.com>
2024-08-07 09:44:57 +0000

    update services/clouddisk_database/src/clouddisk_rdbstore.cpp.
    Signed-off-by: xuerunlong <xuerunlong1@huawei.com>

--- services/clouddisk_database/src/clouddisk_rdbstore.cpp
+++ services/clouddisk_database/src/clouddisk_rdbstore.cpp
@@ -845,12 +845,7 @@ int32_t CloudDiskRdbStore::GetExtAttrValue(const std::string &cloudId, const std
         return E_RDB;
     }
 
-    if (!nlohmann::json::accept(res)) {
-        LOGE("get ext jsonObj accept failed");
-        return E_RDB;
-    }
-
-    nlohmann::json jsonObj = nlohmann::json::parse(res);
+    nlohmann::json jsonObj = nlohmann::json::parse(res, nullptr, false);
     if (jsonObj.is_discarded()) {
         LOGE("get ext jsonObj parse failed");
         return E_RDB;
@@ -928,11 +923,7 @@ static int32_t ExtAttributeSetValue(const std::string &key, const std::string &v
     if (xattrList.empty()) {
         jsonObj = nlohmann::json({{key, value}});
     } else {
-        if (!nlohmann::json::accept(xattrList)) {
-            LOGE("ext jsonObj accept failed");
-            return E_RDB;
-        }
-        jsonObj = nlohmann::json::parse(xattrList);
+        jsonObj = nlohmann::json::parse(xattrList, nullptr, false);
         if (jsonObj.is_discarded()) {
             LOGE("ext jsonObj parse failed");
             return E_RDB;
