64d1506758f198d6ff9d2b3f9955d856a5d38ae0
liuhaotian <liuhaotian15@huawei.com>
2024-12-27 15:50:56 +0800

    adapt uv work
    Signed-off-by: liuhaotian <liuhaotian15@huawei.com>
    Change-Id: Ia6b032a1a7416ec709e932cb78f48d0a9971414b

--- interfaces/kits/js/cloudfilesync/cloud_sync_napi.h
+++ interfaces/kits/js/cloudfilesync/cloud_sync_napi.h
@@ -139,8 +139,7 @@ public:
 
     ~ChangeListenerNapi(){};
     void OnChange(CloudChangeListener &listener, const napi_ref cbRef);
-    int32_t UvQueueWork(uv_loop_s *loop, uv_work_t *work);
-    static napi_value SolveOnChange(napi_env env, UvChangeMsg *msg);
+    static napi_value SolveOnChange(napi_env env, const AAFwk::ChangeInfo &changeInfo);
     napi_ref cbOnRef_ = nullptr;
     napi_ref cbOffRef_ = nullptr;
     std::vector<std::shared_ptr<CloudNotifyObserver>> observers_;
