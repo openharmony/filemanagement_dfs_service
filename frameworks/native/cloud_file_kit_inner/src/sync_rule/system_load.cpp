3d5352dee38710e1fc3fd2a708bd0c29922e8e1f
18721213663 <zhuhongtao12@huawei.com>
2025-01-07 16:29:50 +0800

    download thumb without charging
    Signed-off-by: 18721213663 <zhuhongtao12@huawei.com>

--- frameworks/native/cloud_file_kit_inner/src/sync_rule/system_load.cpp
+++ frameworks/native/cloud_file_kit_inner/src/sync_rule/system_load.cpp
@@ -88,9 +88,9 @@ bool SystemLoadStatus::IsLoadStatusUnderNormal(STOPPED_TYPE process)
     return true;
 }
 
-bool SystemLoadStatus::IsLoadStatusUnderHot(STOPPED_TYPE process)
+bool SystemLoadStatus::IsLoadStatusUnderHot(STOPPED_TYPE process, int32_t level)
 {
-    if (loadstatus_ > SYSTEMLOADLEVEL_HOT) {
+    if (loadstatus_ > level) {
         if (process == STOPPED_IN_THUMB) {
             LOGI("SetParameter TEMPERATURE_SYSPARAM_THUMB true");
             system::SetParameter(TEMPERATURE_SYSPARAM_THUMB, "true");
