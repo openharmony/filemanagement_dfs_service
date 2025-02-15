9a9f7fb888504e53089d22374c1f7b92cbc5f73a
guomeicen <guomeicen@huawei.com>
2025-02-15 08:19:44 +0000

    挑单5.0.1
    Signed-off-by: guomeicen <guomeicen@huawei.com>

--- services/cloudfiledaemon/src/fuse_manager/fuse_manager.cpp
+++ services/cloudfiledaemon/src/fuse_manager/fuse_manager.cpp
@@ -1417,7 +1417,7 @@ static void CloudRead(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
             FaultType::FILE, ENOMEM, "buffer is null"});
         return;
     }
-    if (cInode->mBase->hasDownloaded) {
+    if (cInode->mBase->hasDownloaded && fi->fh ！= UINT64_MAX) {
         CloudReadOnLocalFile(req, buf, size, off, fi);
         return;
     }
