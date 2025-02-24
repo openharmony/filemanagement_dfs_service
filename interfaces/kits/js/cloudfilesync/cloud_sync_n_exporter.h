02f0503bb2f969db006b20eb8791c9f6f015352f
y00854865 <yangfan49@huawei.com>
2025-02-24 12:10:40 +0800

    黄蓝差异修改
    Signed-off-by: y00854865 <yangfan49@huawei.com>

--- interfaces/kits/js/cloudfilesync/cloud_sync_n_exporter.h
+++ interfaces/kits/js/cloudfilesync/cloud_sync_n_exporter.h
@@ -22,8 +22,8 @@ namespace OHOS::FileManagement::CloudSync {
 void InitCloudSyncState(napi_env env, napi_value exports);
 void InitFileSyncState(napi_env env, napi_value exports);
 void InitErrorType(napi_env env, napi_value exports);
-void InitDownloadErrorType(napi_env env, napi_value exports);
 void InitCloudSyncApi(napi_env env, napi_value exports);
+void InitDownloadErrorType(napi_env env, napi_value exports);
 void InitNotifyType(napi_env env, napi_value exports);
 void InitCloudSyncFuncs(napi_env env, napi_value exports);
 } // namespace OHOS::FileManagement::CloudSync
