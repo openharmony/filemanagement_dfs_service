a248124fbd15bd949197ee0ab111d51ea0727852
reminder2352 <mabiao21@huawei.com>
2025-03-05 16:19:10 +0800

    add core code.
    Signed-off-by: reminder2352 <mabiao21@huawei.com>
    Change-Id: I9c7c86ba9a11629579bc6f7fa081b3ab7685e80f

---
+++ interfaces/kits/js/cloudfilesync/cloud_sync_core.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_FILEMGMT_CLOUD_SYNC_CORE_H
+#define OHOS_FILEMGMT_CLOUD_SYNC_CORE_H
+
+#include <optional>
+
+#include "cloud_sync_callback_ani.h"
+#include "filemgmt_libfs.h"
+
+namespace OHOS::FileManagement::CloudSync {
+using namespace ModuleFileIO;
+
+struct BundleEntity {
+    std::string bundleName_;
+    explicit BundleEntity(const std::string bundleName) : bundleName_(bundleName) {};
+};
+
+class CloudSyncCore {
+public:
+    static FsResult<CloudSyncCore *> Constructor(const std::string &bundleName);
+
+    FsResult<void> DoStart();
+    FsResult<void> DoStop();
+    FsResult<void> DoOn(const std::string &event, const std::shared_ptr<CloudSyncCallbackMiddle> callback);
+    FsResult<void> DoOff(const std::string &event,
+        const std::optional<std::shared_ptr<CloudSyncCallbackMiddle>> &callback = std::nullopt);
+
+    const std::string &GetBundleName() const;
+    explicit CloudSyncCore(const std::string &bundleName);
+    ~CloudSyncCore() = default;
+
+private:
+    std::shared_ptr<CloudSyncCallbackMiddle> callback_;
+    std::unique_ptr<BundleEntity> bundleEntity;
+};
+} // namespace OHOS::FileManagement::CloudSync
+#endif // OHOS_FILEMGMT_CLOUD_SYNC_CORE_H
