0fec0ff068d103342ce5a324116739eff8448bcd
liuxiaowei <liuxiaowei45@huawei.com>
2025-03-11 16:43:01 +0800

    修改多用户多次切换用户之后，dfs出现建链失败问题
    Signed-off-by: liuxiaowei <liuxiaowei45@huawei.com>

--- utils/log/include/dfs_error.h
+++ utils/log/include/dfs_error.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -131,6 +131,7 @@ enum DFSErrCode {
     ERR_DLOPEN,
     ERR_PUBLISH_STATE,
     ERR_ALLCONNECT,
+    ERR_CHECKOUT_COUNT,
 
     E_PERM,
     E_NOENT,
