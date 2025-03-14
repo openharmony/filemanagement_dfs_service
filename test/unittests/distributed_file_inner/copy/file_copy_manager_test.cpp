6eedb1c5d66ae0d4184d209f3aec7bbaec6ac993
宁竞莜 <ningjingyou@huawei.com>
2025-03-14 15:25:01 +0800

    UT add Signed-off-by: 宁竞莜 <ningjingyou@huawei.com>

--- test/unittests/distributed_file_inner/copy/file_copy_manager_test.cpp
+++ test/unittests/distributed_file_inner/copy/file_copy_manager_test.cpp
@@ -11,7 +11,7 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
- */
+*/
 
 #include <copy/file_copy_manager.h>
 #include <gtest/gtest.h>
