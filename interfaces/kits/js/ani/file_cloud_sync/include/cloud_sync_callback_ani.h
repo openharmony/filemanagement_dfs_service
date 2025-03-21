e1b4817b7e77d9c5920569884067161b35dfae5e
reminder2352 <mabiao21@huawei.com>
2025-03-21 17:48:49 +0800

    add register change data on callback
    Signed-off-by: reminder2352 <mabiao21@huawei.com>
    Change-Id: Icf02a6ee8cd8e6ee3b4e8cb946bedf37ac2600ec

--- interfaces/kits/js/ani/file_cloud_sync/include/cloud_sync_callback_ani.h
+++ interfaces/kits/js/ani/file_cloud_sync/include/cloud_sync_callback_ani.h
@@ -19,8 +19,46 @@
 #include <ani.h>
 
 #include "cloud_sync_callback_middle.h"
+#include "data_ability_observer_interface.h"
+#include "data_ability_observer_stub.h"
+#include "dataobs_mgr_client.h"
+#include "securec.h"
+#include "uri.h"
 
 namespace OHOS::FileManagement::CloudSync {
+
+class CloudNotifyObserver;
+
+struct CloudChangeListener {
+    NotifyType notifyType;
+    AAFwk::ChangeInfo changeInfo;
+    std::string strUri;
+};
+
+struct RegisterParams {
+    std::string uri;
+    bool recursion;
+    ani_ref cbOnRef;
+};
+
+class ObserverImpl : public AAFwk::DataAbilityObserverStub {
+public:
+    explicit ObserverImpl(const std::shared_ptr<CloudNotifyObserver> cloudNotifyObserver)
+        : cloudNotifyObserver_(cloudNotifyObserver) {};
+    void OnChange();
+    void OnChangeExt(const AAFwk::ChangeInfo &info);
+    static sptr<ObserverImpl> GetObserver(const Uri &uri, const std::shared_ptr<CloudNotifyObserver> &observer);
+    static bool FindObserver(const Uri &uri, const std::shared_ptr<CloudNotifyObserver> &observer);
+    static bool DeleteObserver(const Uri &uri, const std::shared_ptr<CloudNotifyObserver> &observer);
+
+private:
+    struct ObserverParam {
+        sptr<ObserverImpl> obs_;
+        std::list<Uri> uris_;
+    };
+    std::shared_ptr<CloudNotifyObserver> cloudNotifyObserver_;
+    static ConcurrentMap<CloudNotifyObserver *, ObserverParam> observers_;
+};
 
 class CloudSyncCallbackAniImpl : public CloudSyncCallbackMiddle,
                                  public std::enable_shared_from_this<CloudSyncCallbackAniImpl> {
@@ -34,6 +72,53 @@ public:
 private:
     ani_env *env_;
     ani_ref cbOnRef_ = nullptr;
+};
+
+class ChangeListenerAni {
+public:
+    explicit ChangeListenerAni(ani_env *env) : env_(env) {}
+
+    ChangeListenerAni(const ChangeListenerAni &listener)
+    {
+        this->env_ = listener.env_;
+        this->cbOnRef_ = listener.cbOnRef_;
+        this->cbOffRef_ = listener.cbOffRef_;
+        this->observers_ = listener.observers_;
+    }
+
+    ChangeListenerAni &operator=(const ChangeListenerAni &listener)
+    {
+        this->env_ = listener.env_;
+        this->cbOnRef_ = listener.cbOnRef_;
+        this->cbOffRef_ = listener.cbOffRef_;
+        this->observers_ = listener.observers_;
+        return *this;
+    }
+
+    ~ChangeListenerAni() {};
+    void OnChange(CloudChangeListener &listener, const ani_ref cbRef);
+    ani_ref cbOnRef_ = nullptr;
+    ani_ref cbOffRef_ = nullptr;
+    std::vector<std::shared_ptr<CloudNotifyObserver>> observers_;
+
+private:
+    ani_env *env_;
+};
+
+class CloudNotifyObserver {
+public:
+    CloudNotifyObserver(const ChangeListenerAni &listObj, std::string uri, ani_ref ref)
+        : listObj_(listObj), uri_(uri), ref_(ref)
+    {
+    }
+    ~CloudNotifyObserver() = default;
+
+    void OnChange();
+    void OnchangeExt(const AAFwk::ChangeInfo &changeInfo);
+
+    ChangeListenerAni listObj_;
+    std::string uri_;
+    ani_ref ref_;
 };
 } // namespace OHOS::FileManagement::CloudSync
 #endif // OHOS_FILEMGMT_CLOUD_SYNC_CALLBACK_ANI_H
--- interfaces/kits/js/ani/file_cloud_sync/src/cloud_sync_callback_ani.cpp
+++ interfaces/kits/js/ani/file_cloud_sync/src/cloud_sync_callback_ani.cpp
@@ -17,6 +17,67 @@
 #include "utils_log.h"
 
 namespace OHOS::FileManagement::CloudSync {
+using namespace std;
+
+static const unsigned int READ_SIZE = 1024;
+static mutex obsMutex_;
+
+ConcurrentMap<CloudNotifyObserver *, ObserverImpl::ObserverParam> ObserverImpl::observers_;
+
+void ObserverImpl::OnChange() {}
+
+void ObserverImpl::OnChangeExt(const AAFwk::ChangeInfo &info)
+{
+    if (cloudNotifyObserver_ == nullptr) {
+        LOGE("cloudNotifyObserver_ is null!");
+        return;
+    }
+    cloudNotifyObserver_->OnchangeExt(info);
+}
+
+sptr<ObserverImpl> ObserverImpl::GetObserver(const Uri &uri, const shared_ptr<CloudNotifyObserver> &observer)
+{
+    lock_guard<mutex> lock(obsMutex_);
+    sptr<ObserverImpl> result = nullptr;
+    observers_.Compute(observer.get(), [&result, &uri, &observer](const auto &key, auto &value) {
+        if (value.obs_ == nullptr) {
+            value.obs_ = new (nothrow) ObserverImpl(observer);
+            value.uris_.push_back(uri);
+        } else {
+            auto it = find(value.uris_.begin(), value.uris_.end(), uri);
+            if (it == value.uris_.end()) {
+                value.uris_.push_back(uri);
+            }
+        }
+
+        result = value.obs_;
+        return result != nullptr;
+    });
+
+    return result;
+}
+
+bool ObserverImpl::FindObserver(const Uri &uri, const shared_ptr<CloudNotifyObserver> &observer)
+{
+    lock_guard<mutex> lock(obsMutex_);
+    auto result = observers_.Find(observer.get());
+    if (result.first) {
+        auto it = std::find(result.second.uris_.begin(), result.second.uris_.end(), uri);
+        if (it == result.second.uris_.end()) {
+            return false;
+        }
+    }
+    return result.first;
+}
+
+bool ObserverImpl::DeleteObserver(const Uri &uri, const shared_ptr<CloudNotifyObserver> &observer)
+{
+    lock_guard<mutex> lock(obsMutex_);
+    return observers_.ComputeIfPresent(observer.get(), [&uri](auto &key, auto &value) {
+        value.uris_.remove_if([&uri](const auto &value) { return uri == value; });
+        return !value.uris_.empty();
+    });
+}
 
 CloudSyncCallbackAniImpl::CloudSyncCallbackAniImpl(ani_env *env, ani_ref fun) : env_(env)
 {
@@ -95,5 +156,199 @@ void CloudSyncCallbackAniImpl::DeleteReference()
 void CloudSyncCallbackAniImpl::OnSyncStateChanged(SyncType type, SyncPromptState state)
 {
     return;
+}
+
+static ani_status SetValueArray(ani_env *env, const std::list<Uri> listValue, ani_object &uris)
+{
+    ani_class arrayCls = nullptr;
+    ani_status ret = env->FindClass("Lescompat/Array;", &arrayCls);
+    if (ret != ANI_OK) {
+        LOGE("find ani array failed. ret = %{public}d", static_cast<int32_t>(ret));
+        return ret;
+    }
+
+    ani_method arrayCtor;
+    ret = env->Class_FindMethod(arrayCls, "<ctor>", "I:V", &arrayCtor);
+    if (ret != ANI_OK) {
+        LOGE("array find method failed. ret = %{public}d", static_cast<int32_t>(ret));
+        return ret;
+    }
+
+    std::vector<std::string> strings(listValue.size());
+    int elementIndex = 0;
+    for (auto uri : listValue) {
+        strings[elementIndex++] = uri.ToString();
+    }
+
+    ret = env->Object_New(arrayCls, arrayCtor, &uris, strings.size());
+    if (ret != ANI_OK) {
+        LOGE("set array uri new object failed. ret = %{public}d", static_cast<int32_t>(ret));
+        return ret;
+    }
+
+    ani_size index = 0;
+    for (auto str : strings) {
+        ani_string ani_str;
+        ret = env->String_NewUTF8(str.c_str(), str.size(), &ani_str);
+        if (ret != ANI_OK) {
+            LOGE("std string to ani string failed. ret = %{public}d", static_cast<int32_t>(ret));
+            return ret;
+        }
+        ret = env->Object_CallMethodByName_Void(uris, "$_set", "ILstd/core/Object;:V", index, ani_str);
+        if (ret != ANI_OK) {
+            LOGE("set array uri value failed. ret = %{public}d", static_cast<int32_t>(ret));
+            return ret;
+        }
+        index++;
+    }
+    return ANI_OK;
+}
+
+static ani_status SetIsDir(ani_env *env, const shared_ptr<MessageParcel> parcel, ani_object &isDirectory)
+{
+    uint32_t len = 0;
+    if (!parcel->ReadUint32(len)) {
+        LOGE("Failed to read sub uri list length");
+        return ANI_INVALID_ARGS;
+    }
+
+    if (len > READ_SIZE) {
+        return ANI_INVALID_ARGS;
+    }
+
+    ani_class arrayCls = nullptr;
+    ani_status ret = env->FindClass("Lescompat/Array;", &arrayCls);
+    if (ret != ANI_OK) {
+        LOGE("find ani array failed. ret = %{public}d", static_cast<int32_t>(ret));
+        return ret;
+    }
+
+    ani_method arrayCtor;
+    ret = env->Class_FindMethod(arrayCls, "<ctor>", "I:V", &arrayCtor);
+    if (ret != ANI_OK) {
+        LOGE("array find method failed. ret = %{public}d", static_cast<int32_t>(ret));
+        return ret;
+    }
+
+    std::vector<bool> isDirs(len);
+    for (uint32_t i = 0; i < len; i++) {
+        isDirs[i] = parcel->ReadBool();
+    }
+
+    ret = env->Object_New(arrayCls, arrayCtor, &isDirectory, isDirs.size());
+    if (ret != ANI_OK) {
+        LOGE("set array uri new object failed. ret = %{public}d", static_cast<int32_t>(ret));
+        return ret;
+    }
+
+    ani_size index = 0;
+    for (auto isDir : isDirs) {
+        ret = env->Object_CallMethodByName_Void(
+            isDirectory, "$_set", "ILstd/core/Object;:V", index, static_cast<ani_boolean>(isDir));
+        if (ret != ANI_OK) {
+            LOGE("set array uri value failed. ret = %{public}d", static_cast<int32_t>(ret));
+            return ret;
+        }
+        index++;
+    }
+    return ANI_OK;
+}
+
+static ani_status GetChangeDataObject(
+    ani_env *env, CloudChangeListener &listener, ani_class cls, ani_object &changeData)
+{
+    ani_object uris;
+    ani_status ret = SetValueArray(env, listener.changeInfo.uris_, uris);
+    if (ret != ANI_OK) {
+        LOGE("set array uris failed. ret = %{public}d", ret);
+        return ret;
+    }
+
+    ani_object isDirectory;
+    if (listener.changeInfo.size_ > 0) {
+        shared_ptr<MessageParcel> parcel = make_shared<MessageParcel>();
+        if (parcel->ParseFrom(reinterpret_cast<uintptr_t>(listener.changeInfo.data_), listener.changeInfo.size_)) {
+            ret = SetIsDir(env, parcel, isDirectory);
+            if (ret != ANI_OK) {
+                LOGE("Set subArray named property error! field: subUris");
+                return ret;
+            }
+        }
+    }
+
+    ani_enum notifyTypeEnum;
+    env->FindEnum("L@ohos/file/cloudSync/cloudSync/NotifyType;", &notifyTypeEnum);
+    ani_enum_item notifyTypeEnumItem;
+    env->Enum_GetEnumItemByIndex(notifyTypeEnum, listener.changeInfo.changeType_, &notifyTypeEnumItem);
+
+    ani_method ctor;
+    ret = env->Class_FindMethod(
+        cls, "<ctor>", "L@ohos/file/cloudSync/cloudSync/NotifyType;Lescompat/Array;Lescompat/Array;:V", &ctor);
+    if (ret != ANI_OK) {
+        LOGE("find ctor method failed. ret = %{public}d", ret);
+        return ret;
+    }
+
+    ret = env->Object_New(cls, ctor, &changeData, notifyTypeEnumItem, isDirectory, uris);
+    if (ret != ANI_OK) {
+        LOGE("create new object failed. ret = %{public}d", ret);
+        return ret;
+    }
+    return ANI_OK;
+}
+
+void ChangeListenerAni::OnChange(CloudChangeListener &listener, const ani_ref cbRef)
+{
+    ani_size nr_refs = 16;
+    ani_status ret = env_->CreateLocalScope(nr_refs);
+    if (ret != ANI_OK) {
+        LOGE("crete local scope failed. ret = %{public}d", ret);
+        return;
+    }
+
+    if (listener.changeInfo.uris_.empty()) {
+        LOGI("listener uri is empty.");
+        return;
+    }
+
+    ani_namespace ns {};
+    ret = env_->FindNamespace("L@ohos/file/cloudSync/cloudSync;", &ns);
+    if (ret != ANI_OK) {
+        LOGE("find namespace failed. ret = %{public}d", ret);
+        return;
+    }
+    static const char *className = "LChangeDataInner;";
+    ani_class cls;
+    ret = env_->Namespace_FindClass(ns, className, &cls);
+    if (ret != ANI_OK) {
+        LOGE("find class failed. ret = %{public}d", ret);
+        return;
+    }
+
+    ani_object changeData;
+    ret = GetChangeDataObject(env_, listener, cls, changeData);
+
+    ani_ref ref_;
+    ret = env_->Object_CallMethodByName_Ref(
+        static_cast<ani_object>(cbOnRef_), "invoke0", "Lstd/core/Object;:Lstd/core/Object;", &ref_, changeData);
+    if (ret != ANI_OK) {
+        LOGE("ani call function failed. ret = %{public}d", ret);
+        return;
+    }
+
+    ret = env_->DestroyLocalScope();
+    if (ret != ANI_OK) {
+        LOGE("failed to DestroyLocalScope. ret = %{public}d", ret);
+    }
+}
+
+void CloudNotifyObserver::OnChange() {}
+
+void CloudNotifyObserver::OnchangeExt(const AAFwk::ChangeInfo &changeInfo)
+{
+    CloudChangeListener listener;
+    listener.changeInfo = changeInfo;
+    listener.strUri = uri_;
+    listObj_.OnChange(listener, ref_);
 }
 } // namespace OHOS::FileManagement::CloudSync
