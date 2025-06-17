a1e817026decb09ae4ee3fcb36a590d06b6e8e4c
宁竞莜 <ningjingyou@huawei.com>
2025-06-17 10:51:59 +0800

    DFS 分布式文件管理 资源文件和接口定义新增
    Signed-off-by: 宁竞莜 <ningjingyou@huawei.com>

--- services/distributedfiledaemon/include/channel_manager/channel_manager.h
+++ services/distributedfiledaemon/include/channel_manager/channel_manager.h
@@ -1,8 +1,119 @@
-//
-// Created by baishunqin on 2025/6/17.
-//
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef FILEMANAGEMENT_DFS_SERVICE_CHANNEL_MANAGER_H
 #define FILEMANAGEMENT_DFS_SERVICE_CHANNEL_MANAGER_H
 
+#include "control_cmd_parser.h"
+#include "event_handler.h"
+#include "single_instance.h"
+#include "socket.h"
+
+#include <atomic>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <shared_mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace OHOS {
+namespace Storage {
+namespace DistributedFile {
+
+class ChannelManager final {
+    DECLARE_SINGLE_INSTANCE_BASE(ChannelManager);
+
+public:
+    int32_t Init();
+    void DeInit();
+
+    int32_t GetVersion();
+    int32_t CreateClientChannel(const std::string &networkId);
+    int32_t CancelClientChannel(const std::string &networkId);
+    bool HasExistChannel(const std::string &networkId);
+
+    int32_t
+        SendRequest(const std::string &networkId, ControlCmd &request, ControlCmd &response, bool needResponse = false);
+    int32_t SendBytes(const std::string &networkId, const std::string &data);
+    int32_t DoSendBytes(const std::int32_t socketId, const std::string &data);
+
+    void OnSocketError(int32_t socketId, const int32_t errorCode);
+    void OnSocketConnected(int32_t socketId, const PeerSocketInfo &info);
+    void OnSocketClosed(int32_t socketId, const ShutdownReason &reason);
+    void OnBytesReceived(int32_t socketId, const void *data, const uint32_t dataLen);
+
+private:
+    explicit ChannelManager() = default;
+    ~ChannelManager();
+
+    void StartEvent();
+    void StartCallbackEvent();
+    int32_t PostTask(const AppExecFwk::InnerEvent::Callback &callback,
+                     const AppExecFwk::EventQueue::Priority priority,
+                     const std::string &name = "");
+    int32_t PostCallbackTask(const AppExecFwk::InnerEvent::Callback &callback,
+                             const AppExecFwk::EventQueue::Priority priority);
+
+    int32_t CreateServerSocket();
+    int32_t CreateClientSocket(const std::string &peerNetworkId);
+
+private:
+    int32_t VERSION_ = 60;
+    int32_t serverSocketId_ = -1;
+    std::string ownerName_ = "channel_manager";
+    static inline const std::string SERVICE_NAME = "ohos.storage.distributedfile.daemon";
+    static inline const std::string SESSION_NAME = "DistributedFileService_ChannelManager";
+
+    // networkId -> socketId (client)
+    std::shared_mutex clientMutex_;
+    std::map<std::string, int32_t> clientNetworkSocketMap_;
+    std::map<std::string, std::string> clientChannelIdMap_;
+
+    // networkId -> socketId (server)
+    std::shared_mutex serverMutex_;
+    std::map<std::string, int32_t> serverNetworkSocketMap_;
+    std::map<int32_t, std::string> serverChannelIdMap_;
+
+    // for send data
+    std::mutex eventMutex_;
+    std::thread eventThread_;
+    std::shared_ptr<OHOS::AppExecFwk::EventHandler> eventHandler_;
+    std::condition_variable eventCon_;
+
+    // for receive data and softbus callback
+    std::mutex callbackEventMutex_;
+    std::thread callbackEventThread_;
+    std::shared_ptr<OHOS::AppExecFwk::EventHandler> callbackEventHandler_;
+    std::condition_variable callbackEventCon_;
+
+    struct ResponseWaiter {
+        std::mutex mutex;
+        std::condition_variable cv;
+        ControlCmd response;
+        bool received = false;
+    };
+
+    // for control cmd
+    std::mutex mtx_;
+    std::condition_variable cmdEventCon_;
+    std::unordered_map<int32_t, std::shared_ptr<ResponseWaiter>> pendingResponses_;
+};
+} // namespace DistributedFile
+} // namespace Storage
+} // namespace OHOS
 #endif // FILEMANAGEMENT_DFS_SERVICE_CHANNEL_MANAGER_H
--- services/distributedfiledaemon/include/channel_manager/control_cmd_parser.h
+++ services/distributedfiledaemon/include/channel_manager/control_cmd_parser.h
@@ -1,8 +1,77 @@
-//
-// Created by baishunqin on 2025/6/17.
-//
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef FILEMANAGEMENT_DFS_SERVICE_CONTROL_CMD_PARSER_H
 #define FILEMANAGEMENT_DFS_SERVICE_CONTROL_CMD_PARSER_H
+
+#include <atomic>
+#include <functional>
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace OHOS {
+namespace Storage {
+namespace DistributedFile {
+
+using json = nlohmann::json;
+
+static const char DETERMINE_DEVICE_TYPE_KEY[] = "persist.distributed_scene.sys_settings_data_sync";
+
+enum ControlCmdType {
+    CMD_UNKNOWN = 0,
+    CMD_CHECK_ALLOW_CONNECT = 1,
+    CMD_RESPONSE = 2,
+    CMD_PUBLISH_NOTIFICATION = 3,
+    CMD_CANCEL_NOTIFICATION = 4,
+    CMD_DISCONNECT = 5,
+};
+
+struct ControlCmd {
+    static std::atomic<int32_t> nextMsgId; // 声明静态成员
+
+    int32_t version = 0;
+    int32_t msgId = 0;
+    int32_t msgType = 0;
+    std::string msgBody = "";
+    std::string networkId = "";
+
+    ControlCmd() : msgId(nextMsgId.fetch_add(1, std::memory_order_relaxed)) {}
+};
+
+inline std::atomic<int32_t> ControlCmd::nextMsgId;
+
+class ControlCmdParser {
+public:
+    static bool ParseFromJson(const std::string &jsonStr, ControlCmd &outCmd);
+    static bool SerializeToJson(const ControlCmd &cmd, std::string &outJsonStr);
+
+    static bool HandleRequest(ControlCmd &inCmd, ControlCmd &outCmd);
+
+    static bool CheckAllowConnect(ControlCmd &inCmd, ControlCmd &outCmd);
+    static bool PublishNotification(ControlCmd &inCmd, ControlCmd &outCmd);
+    static bool CancelNotification(ControlCmd &inCmd, ControlCmd &outCmd);
+    static bool DisconnectByRemote(ControlCmd &inCmd, ControlCmd &outCmd);
+
+    static void RegisterDisconnectCallback(std::function<void(std::string &)> cb);
+
+private:
+    static std::function<void(std::string &)> callback_; // 私有静态成员
+};
+
+} // namespace DistributedFile
+} // namespace Storage
+} // namespace OHOS
 
 #endif // FILEMANAGEMENT_DFS_SERVICE_CONTROL_CMD_PARSER_H
