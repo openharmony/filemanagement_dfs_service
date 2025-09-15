67e80289e82e601939eb4b7eeea5db9b308e7389
wangdengyang <wangdengyang@noreply.gitcode.com>
2025-09-15 17:09:06 +0800

    RemovedClean
    Signed-off-by: wangdengyang <wangdengyang1@h-partners.com>

--- test/unittests/cloudsync_api/cloudsync_impl/include/service_proxy_mock.h
+++ test/unittests/cloudsync_api/cloudsync_impl/include/service_proxy_mock.h
@@ -103,6 +103,7 @@ public:
     MOCK_METHOD2(ReplaceFileWithHistoryVersion, int32_t(const string &uri, const string &versionUri));
     MOCK_METHOD2(IsFileConflict, int32_t(const string &uri, bool &isConflict));
     MOCK_METHOD1(ClearFileConflict, int32_t(const string &uri));
+    MOCK_METHOD2(RemovedClean, ErrCode(const std::string& bundleName, int32_t userId));
 
 private:
     int32_t StartFileCacheWriteParcel(MessageParcel &data,
