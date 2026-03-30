095a31091d2c4042dbf14ba87d670a9842d411f7
Curremy <dingzeyu2@h-partners.com>
2026-03-30 14:52:25 +0800

    云同步规格裁剪适配儿童手表
    Signed-off-by: Curremy <dingzeyu2@h-partners.com>

--- interfaces/kits/js/ani/file_cloud_sync/src/cloud_file_cache_ani.cpp
+++ interfaces/kits/js/ani/file_cloud_sync/src/cloud_file_cache_ani.cpp
@@ -341,14 +341,14 @@ ani_long CloudFileCacheAni::CloudFileCacheStartBatchForWatch(ani_env *env, ani_o
     return 0;
 }
 
-void CloudFileCacheAni::CloudFileCacheStopForWatch(ani_env *env, ani_object object, ani_string uri, 
+void CloudFileCacheAni::CloudFileCacheStopForWatch(ani_env *env, ani_object object, ani_string uri,
     ani_boolean needClean)
 {
     LOGD("Start CloudFileCacheStopForWatch in ani");
     return;
 }
 
-void CloudFileCacheAni::CloudFileCacheStopBatchForWatch(ani_env *env, ani_object object, ani_long taskId, 
+void CloudFileCacheAni::CloudFileCacheStopBatchForWatch(ani_env *env, ani_object object, ani_long taskId,
     ani_boolean needClean)
 {
     LOGD("Start CloudFileCacheStopBatchForWatch in ani");
