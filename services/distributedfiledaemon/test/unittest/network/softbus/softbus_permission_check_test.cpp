a118a44c6f31ef89860c4cd660a50140ef38fd12
lishang21 <lishang21@h-partners.com>
2026-08-26 10:21:55 +0800

    update: 更新文件 softbus_permission_check_test.cpp
    Signed-off-by: lishang21 <lishang21@h-partners.com>

--- services/distributedfiledaemon/test/unittest/network/softbus/softbus_permission_check_test.cpp
+++ services/distributedfiledaemon/test/unittest/network/softbus/softbus_permission_check_test.cpp
@@ -42,7 +42,8 @@ public:
     static void SetUpTestCase(void);
     static void TearDownTestCase(void);
     void SetUp() {};
-    void TearDown() {
+    void TearDown()
+    {
         ::testing::Mock::VerifyAndClearExpectations(deviceManagerImplMock_.get());
     };
 public:
