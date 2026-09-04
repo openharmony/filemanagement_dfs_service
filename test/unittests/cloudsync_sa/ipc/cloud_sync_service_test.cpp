9c89f510b67a1fc1c1af02b94164c464d530da8e
zerodragon <gengchen2@h-partners.com>
2026-09-04 09:59:35 +0800

    update: 更新文件 cloud_sync_service_test.cpp
    Signed-off-by: zerodragon <gengchen2@h-partners.com>

--- test/unittests/cloudsync_sa/ipc/cloud_sync_service_test.cpp
+++ test/unittests/cloudsync_sa/ipc/cloud_sync_service_test.cpp
@@ -664,7 +664,7 @@ HWTEST_F(CloudSyncServiceTest, CleanCacheInnerWithBundleNameTest004, TestSize.Le
     GTEST_LOG_(INFO) << "CleanCacheInnerWithBundleNameTest001 start";
     try {
         EXPECT_NE(servicePtr_, nullptr);
-        EXPECT_CALL(*dfsuAccessToken_, GetCallerBundleName(_)).WillOnce(Return(E_OK));
+
         int32_t ret = servicePtr_->CleanAllFileCacheInner();
         EXPECT_EQ(ret, E_OK);
     } catch (...) {
@@ -686,7 +686,7 @@ HWTEST_F(CloudSyncServiceTest, CleanCacheInnerWithBundleNameTest005, TestSize.Le
     try {
         EXPECT_NE(servicePtr_, nullptr);
         std::string bundleName = "com.ohos.photos";
-        EXPECT_CALL(*dfsuAccessToken_, GetCallerBundleName(_)).WillOnce(Return(E_OK));
+
         int32_t ret = servicePtr_->CleanAllFileCacheInner();
         EXPECT_EQ(ret, E_OK);
     } catch (...) {
